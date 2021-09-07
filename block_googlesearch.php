4a4584c060ac5a6e2e8eed958320720576582cec
hemkef <felix.hemke@htw-berlin.de>
2021-09-07 11:24:10 +0200

    Search View is now embedded into Moodle Block

--- block_googlesearch.php
+++ block_googlesearch.php
@@ -4,7 +4,6 @@ require_once('googlesearch_form.php');
 class block_googlesearch extends block_base {
     public function init() {
         $this->title = get_string('googlesearch', 'block_googlesearch');
-
     }
     public function get_content() {
         if ($this->content !== null) {
@@ -12,7 +11,15 @@ class block_googlesearch extends block_base {
         }
         $this->content         =  new stdClass;
         $this->content->items  = array();
-        $jsonResponse = file_get_contents("https://customsearch.googleapis.com/customsearch/v1?cx=fd58b2a4eb55453c2&num=10&q=Moodle%20Blocks&key=[API-KEY]]");
+        $searchTermUrlParameter = htmlspecialchars($_GET["searchterm"]);
+        $apiKey = '[API-Key]';
+        if ($searchTermUrlParameter == null) {
+            $jsonResponse = file_get_contents("https://customsearch.googleapis.com/customsearch/v1?cx=fd58b2a4eb55453c2&num=10&q=Moodle%20Blocks&key=".$apiKey);
+        }
+        else {
+            $jsonResponse = file_get_contents("https://customsearch.googleapis.com/customsearch/v1?cx=fd58b2a4eb55453c2&num=10&q='.$searchTermUrlParameter.'&key=".$apiKey);
+        }
+
         $jsonDecoded = json_decode($jsonResponse, true);
         $titles = array();
         $links = array();
@@ -28,8 +35,13 @@ class block_googlesearch extends block_base {
         }
         $this->content->text = $resultString;
 
-        $url = new moodle_url('/blocks/googlesearch/view.php');
-        $this->content->footer = html_writer::link($url, "Suchwort ändern");
+        $simplehtml = new googlesearch_form();
+        $this->content->footer =  $simplehtml->render();
+        if ($fromform = $simplehtml->get_data()) {
+            $search = $fromform->{'searchterm'};
+            $url = new moodle_url('/', array('searchterm' => $search));
+            redirect($url);
+        }
         return $this->content;
     }
 }
--- googlesearch_form.php
+++ googlesearch_form.php
@@ -5,20 +5,29 @@ class googlesearch_form extends moodleform {
 
     function definition() {
         global $CFG;
-        /*$current = $this->_customdata['current'];*/
-
+        $searchterm = null;
         $mform =& $this->_form;
         $attributes = array('size' => '50', 'maxlength' => '100');
         $mform->addElement('text', 'searchterm', "Suchwort festlegen", $attributes);
         $this->add_action_buttons();
         $searchterm = $mform->getElement('searchterm');
         $searchtermvalue = $searchterm->getValue();
-/*        $this->_customdata['searchterm'] = $searchtermvalue;
-        $this->set_data(searchterm);*/
+        $this->_customdata['searchterm'] = $searchtermvalue;
+        $this->set_data(searchterm);
 
     }
     function validation($data, $files) {
-        return array();
+        return json_decode($data, true);//array();
     }
 
+    public function definition_after_data() {
+        parent::definition_after_data();
+
+        $mform =& $this->_form;
+        $searchterm =& $mform->getElement('searchterm');
+
+        if (isset($searchterm->_attributes['searchterm'])) {
+            $searchterm->attributes['value'] = $mform->getElement('searchterm');
+        }
+    }
 }
